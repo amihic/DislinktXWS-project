d9de9663f4f72415ec7bfb9810ffafd54a34e0cc
amihic <aleksa.mihic99@gmail.com>
2022-10-27 01:05:20 +0200

    UserController - findAll

--- DislinktXWSProject/src/main/java/com/example/DislinktXWSProject/controller/UserController.java
+++ DislinktXWSProject/src/main/java/com/example/DislinktXWSProject/controller/UserController.java
@@ -1,4 +1,6 @@
 package com.example.DislinktXWSProject.controller;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import com.example.DislinktXWSProject.service.UserService;
 import com.example.DislinktXWSProject.model.User;
+import com.example.DislinktXWSProject.repository.UserRepository;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,11 +21,22 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private UserRepository userRepository;
+	
 	@RequestMapping(value="api/user/{id}",method = RequestMethod.GET, produces= {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 	public ResponseEntity<User> getById(@PathVariable Long id){
 		User user = this.userService.findById(id);
 		return new ResponseEntity<>(user, HttpStatus.OK);
 	}
+	
+	@RequestMapping(value="api/users",method = RequestMethod.GET, produces= {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	public ResponseEntity<List<User>> findAll(){
+		List<User> users = this.userRepository.findAll();
+		return new ResponseEntity<>(users, HttpStatus.OK);
+	}
+	
+	
 	
 	
 }
