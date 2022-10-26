85e45b5f7f7f767e4d91ef534beb50c939cb262a
amihic <aleksa.mihic99@gmail.com>
2022-10-27 01:44:21 +0200

    UserController - delete

--- DislinktXWSProject/src/main/java/com/example/DislinktXWSProject/controller/UserController.java
+++ DislinktXWSProject/src/main/java/com/example/DislinktXWSProject/controller/UserController.java
@@ -3,9 +3,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import com.example.DislinktXWSProject.service.UserService;
 import com.example.DislinktXWSProject.model.User;
@@ -24,16 +21,29 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
+	//pronadji usera po id
 	@RequestMapping(value="api/user/{id}",method = RequestMethod.GET, produces= {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 	public ResponseEntity<User> getById(@PathVariable Long id){
 		User user = this.userService.findById(id);
 		return new ResponseEntity<>(user, HttpStatus.OK);
 	}
 	
+	//pronadji sve usere
 	@RequestMapping(value="api/users",method = RequestMethod.GET, produces= {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 	public ResponseEntity<List<User>> findAll(){
 		List<User> users = this.userRepository.findAll();
 		return new ResponseEntity<>(users, HttpStatus.OK);
+	}
+	
+	//izbrisi usera po id
+	@RequestMapping(value = "api/user/{id}",method=RequestMethod.DELETE)
+	public ResponseEntity<User> delete(@PathVariable Long id){
+		User user = this.userService.findById(id);
+		if(user==null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		this.userRepository.delete(user);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 	
 	
