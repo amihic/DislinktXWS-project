e8abdf000b4fc08f59ea1f2a1b06d26ecf01aeed
amihic <aleksa.mihic99@gmail.com>
2022-10-27 00:21:13 +0200

    UserController&UserService - getById

--- DislinktXWSProject/src/main/java/com/example/DislinktXWSProject/controller/UserController.java
+++ DislinktXWSProject/src/main/java/com/example/DislinktXWSProject/controller/UserController.java
@@ -1,17 +1,27 @@
 package com.example.DislinktXWSProject.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.ResponseEntity;
 import com.example.DislinktXWSProject.service.UserService;
+import com.example.DislinktXWSProject.model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("*")
 @RestController
 public class UserController {
 
+	@Autowired
+	private UserService userService;
 	
-	
+	@RequestMapping(value="api/user/{id}",method = RequestMethod.GET, produces= {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	public ResponseEntity<User> getById(@PathVariable Long id){
+		User user = this.userService.findById(id);
+		return new ResponseEntity<>(user, HttpStatus.OK);
+	}
 	
 	
 }
--- DislinktXWSProject/src/main/java/com/example/DislinktXWSProject/service/UserService.java
+++ DislinktXWSProject/src/main/java/com/example/DislinktXWSProject/service/UserService.java
@@ -1,7 +1,23 @@
 package com.example.DislinktXWSProject.service;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.example.DislinktXWSProject.model.User;
+import com.example.DislinktXWSProject.repository.UserRepository;
 
 @Service
 public class UserService {
 
+	@Autowired
+	private UserRepository userRepository;
+	
+	public User findById(Long id)
+    {
+        Optional<User> opt=this.userRepository.findById(id);
+        if(!opt.isPresent()) {
+            return null;
+        }
+        return opt.get();
+    }
+	
 }
